937c57896c1ce4c95d96d93babd8e50023feb0d2
Johnny <thejohnnytran27@gmail.com>
2021-03-03 11:02:36 -0600

    reset redux store on logout

--- react-app/src/components/auth/LogoutButton.js
+++ react-app/src/components/auth/LogoutButton.js
@@ -1,13 +1,14 @@
 import React from 'react';
 import { useDispatch } from 'react-redux';
 import { logout } from '../../services/auth';
-import logout_user from '../../store/user';
+import { logoutUser } from '../../store/user';
 
 const LogoutButton = ({ setAuthenticated }) => {
   const dispatch = useDispatch();
   const onLogout = async (e) => {
     await logout();
     setAuthenticated(false);
+    dispatch(logoutUser());
   };
 
   return <button onClick={onLogout}>Logout</button>;

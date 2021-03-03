b54465968a7093f0362960d261c4c228508d32de
Johnny <thejohnnytran27@gmail.com>
2021-03-02 21:23:22 -0600

    create and seed users table

--- app/models/user.py
+++ app/models/user.py
@@ -1,33 +1,42 @@
 from .db import db
 from werkzeug.security import generate_password_hash, check_password_hash
 from flask_login import UserMixin
+from datetime import datetime
+
 
 class User(db.Model, UserMixin):
-  __tablename__ = 'users'
+    __tablename__ = 'users'
 
-  id = db.Column(db.Integer, primary_key = True)
-  username = db.Column(db.String(40), nullable = False, unique = True)
-  email = db.Column(db.String(255), nullable = False, unique = True)
-  hashed_password = db.Column(db.String(255), nullable = False)
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(40), nullable=False, unique=True)
+    first_name = db.Column(db.String(40), nullable=False)
+    last_name = db.Column(db.String(40), nullable=False)
+    email = db.Column(db.String(255), nullable=False, unique=True)
+    hashed_password = db.Column(db.String(255), nullable=False)
+    coins = db.Column(db.Integer, nullable=False, default=1000)
+    level = db.Column(db.Integer, nullable=False, default=1)
+    exp = db.Column(db.Integer, nullable=False, default=0)
+    created_at = db.Column(db.DateTime, default=datetime.now())
+    updated_at = db.Column(db.DateTime)
 
+    @property
+    def password(self):
+        return self.hashed_password
 
-  @property
-  def password(self):
-    return self.hashed_password
+    @password.setter
+    def password(self, password):
+        self.hashed_password = generate_password_hash(password)
 
+    def check_password(self, password):
+        return check_password_hash(self.password, password)
 
-  @password.setter
-  def password(self, password):
-    self.hashed_password = generate_password_hash(password)
-
-
-  def check_password(self, password):
-    return check_password_hash(self.password, password)
-
-
-  def to_dict(self):
-    return {
-      "id": self.id,
-      "username": self.username,
-      "email": self.email
-    }
+    def to_dict(self):
+        return {
+            "id": self.id,
+            "username": self.username,
+            "email": self.email,
+            "coins": self.coins,
+            "level": self.level,
+            "exp": self.exp,
+            "created_at": self.created_at
+        }

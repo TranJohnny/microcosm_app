79bcda1e76bfcf4136f2e7efd794791fdc01409c
Johnny <thejohnnytran27@gmail.com>
2021-03-05 15:35:48 -0600

    fix email rounded edge

--- react-app/src/components/auth/SignUpForm.js
+++ react-app/src/components/auth/SignUpForm.js
@@ -129,7 +129,7 @@ const SignUpForm = ({ authenticated, setAuthenticated }) => {
                   type="email"
                   autocomplete="email"
                   required
-                  className="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-t-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm"
+                  className="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm"
                   placeholder="Email address"
                   value={email}
                   onChange={updateEmail}

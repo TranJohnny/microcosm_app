026160274b27da6c5d5c444c9ca8b531506f3767
Johnny <thejohnnytran27@gmail.com>
2021-03-11 13:46:11 -0600

    add shadow

--- react-app/src/components/Timeline/UserSideBar.js
+++ react-app/src/components/Timeline/UserSideBar.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 function UserSideBar({ followed }) {
   return (
-    <div className="bg-gray-50 my-4 py-4 px-4 rounded-lg">
+    <div className="bg-gray-50 my-4 py-4 px-4 rounded-lg shadow-md">
       <p>User Sidebar</p>
       <p>Followed:</p>
       {followed &&

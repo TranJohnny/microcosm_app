a94cc45252f565d70de6ca74b6f332b0c921de69
Johnny <thejohnnytran27@gmail.com>
2021-03-26 09:56:13 -0500

    edit footer

--- react-app/src/components/Footer.js
+++ react-app/src/components/Footer.js
@@ -3,7 +3,9 @@ import React from 'react';
 const Footer = () => {
   return (
     <footer className="py-5 bg-white text-center text-indigo flex flex-row items-center justify-between px-12">
-      <div className="">"Always proud... never satisfied." - Words from the Microcosmos</div>
+      <div className="font-semibold text-indigo">
+        "Always proud... never satisfied." - Words from the Microcosmos
+      </div>
       <div className="flex flex-row justify-around hidden md:block">
         <p className="text-white">Ah-ha-ha! You found me!</p>
       </div>
